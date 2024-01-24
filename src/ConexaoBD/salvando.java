1cd5580877f7fcfd40bc8812a32e0745016f2db8
pedroeva <pedrodzprogramacao@gmail.com>
2024-01-23 22:50:14 -0300

    23/01/2024 22:50

--- src/ConexaoBD/salvando.java
+++ src/ConexaoBD/salvando.java
@@ -4,16 +4,32 @@ import Cliente.cadastros;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class salvando{
    public void save(cadastros cadastro){
+       cadastros informacao = new cadastros();
+
        String sql = "insert into registro(nome, sobrenome, cep, email, senha) values (?,?,?,?,?)";
 
        Connection conexao = null;
        PreparedStatement pstm = null;
 
        try {
-            conexao = conexaoBD.conexao();
-       }catch (){
-   }
-}
+           conexao = conexaoBD.conexao();
+
+
+           pstm = (PreparedStatement) conexao.prepareStatement(sql);
+           pstm.setString(1, informacao.getNome());
+           pstm.setString(2,informacao.getSobrenome());
+           pstm.setString(3,informacao.getCEP());
+           pstm.setString(4,informacao.getEmail());
+           pstm.setString(5,informacao.getSenha());
+
+           pstm.execute();
+   } catch (SQLException e) { //qual a diferença entre Exception e e SQLEXception
+           //throw new RuntimeException(e);
+           System.out.println("Problemas no registro de informações no banco de dados");
+
+       }
+   }}
